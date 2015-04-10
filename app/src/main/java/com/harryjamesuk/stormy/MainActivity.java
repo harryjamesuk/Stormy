3aac8e8ffc5558129f61296de1954940e5b73ef4
Harry James <harryjamesuk@users.noreply.github.com>
2015-04-10 15:21:32 +0100

    TEST CODE: Running networking on main thread
    ** :warning: This code causes an error. We are running from the main
    thread synchronously which is a baaad idea but, we'll fix that in the
    next commit. :warning: **
    
    Created TAG for logging.
    Created test latitude and longitude variables for Alcatraz.
    Set forecastUrl.
    ** :warning: NOTE: apiKey has been removed from this GitHub commit - it
    should be manually added. :warning: **
    
    Removed menu code - we're not using menus.
    
    Created new OkHttpClient.
    Created request using OkHttp's Request.Builder with our url. We then
    build the request.
    Created client call for our request.
    Will then try to execute this call and log it verbosely.
    If call fails, catch the exception and log that in error.

--- app/src/main/java/com/harryjamesuk/stormy/MainActivity.java
+++ app/src/main/java/com/harryjamesuk/stormy/MainActivity.java
@@ -2,38 +2,45 @@ package com.harryjamesuk.stormy;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" +
+                latitude + "," + longitude;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        final OkHttpClient client = new OkHttpClient();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
