5bca8e734bf4d1f3f6bb626adf9c5157edc9d8b8
Harry James <harryjamesuk@users.noreply.github.com>
2015-04-11 17:22:01 +0100

    Setting all of our member variables
    Creating new CurrentWeather object.
    Setting all of the member variables in this CurrentWeather object.
    Then, returning the currentWeather variable which is a CurrentWeather
    object.

--- app/src/main/java/com/harryjamesuk/stormy/MainActivity.java
+++ app/src/main/java/com/harryjamesuk/stormy/MainActivity.java
@@ -85,7 +85,17 @@ public class MainActivity extends ActionBarActivity {
         String timezone = forecast.getString("timezone");
         Log.i(TAG, "From JSON: " + timezone);
 
-        return new CurrentWeather();
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+
+        return currentWeather;
     }
 
     private boolean isNetworkAvailable() {
