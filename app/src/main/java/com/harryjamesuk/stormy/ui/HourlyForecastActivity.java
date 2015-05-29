2a8489af07e41297fd0374bcf467e4b8c8489761
Harry James <harryjamesuk@users.noreply.github.com>
2015-05-29 23:00:04 +0100

    HourlyForecastActivity gets the Hour values
    Created new Hour[], mHours.
    
    Will get our intent and then get the Parcelable array we passed in.
    We'll then create a copy of this array, passing in the Parcelable array
    we want to copy, the length of the array, and the type.

--- app/src/main/java/com/harryjamesuk/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/harryjamesuk/stormy/ui/HourlyForecastActivity.java
@@ -1,16 +1,26 @@
 package com.harryjamesuk.stormy.ui;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
 import com.harryjamesuk.stormy.R;
+import com.harryjamesuk.stormy.weather.Hour;
+
+import java.util.Arrays;
 
 public class HourlyForecastActivity extends ActionBarActivity {
+
+    private Hour[] mHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
 
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
     }
 }
