9dbf63697ce5c4a265a9e0ab6dee65b753d56fd2
Harry James <harryjamesuk@users.noreply.github.com>
2015-06-14 20:43:26 +0100

    Finished adapter methods
    First off, created the HourAdapter constructor where we'll get hour
    Hour[] to bind the data with.
    
    In onCreateViewHolder(), we'll create a new view from our
    hourly_list_item using the LayoutInflater. We'll get the context from
    the ViewGroup parent.
    We can then create a new HourViewHolder using this view and return it.
    
    In onBindViewHolder(), we'll run bindHour() off the holder passed in,
    getting the Hour from the position that is also passed in.
    
    In getItemCount(), we'll just return the length of our Hour array.

--- app/src/main/java/com/harryjamesuk/stormy/adapters/HourAdapter.java
+++ app/src/main/java/com/harryjamesuk/stormy/adapters/HourAdapter.java
@@ -1,6 +1,7 @@
 package com.harryjamesuk.stormy.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,19 +15,28 @@ import com.harryjamesuk.stormy.weather.Hour;
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
+    private Hour[] mHours;
+
+    public HourAdapter(Hour[] hours) {
+        mHours = hours;
+    }
+
     @Override
     public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+        HourViewHolder viewHolder = new HourViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(HourViewHolder holder, int position) {
-
+        holder.bindHour(mHours[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHours.length;
     }
 
     public class HourViewHolder extends RecyclerView.ViewHolder {
